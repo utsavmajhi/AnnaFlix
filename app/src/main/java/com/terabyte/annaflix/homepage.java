121833b5bf7625150a242ba91f6756cc9da539f8
utsavmajhi <utsavmajhi@gmail.com>
2020-04-11 15:51:55 +0530

    added homepage with netflix type ui

---
+++ app/src/main/java/com/terabyte/annaflix/homepage.java
@@ -0,0 +1,18 @@
+package com.terabyte.annaflix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+public class homepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+
+    }
+}
