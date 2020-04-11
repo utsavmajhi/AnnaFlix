f294a910507fd6f5fd4f90f9ab4ce4e560fe0db5
utsavmajhi <utsavmajhi@gmail.com>
2020-04-11 20:54:50 +0530

    created all fragments and also the template for single anime item

--- app/src/main/java/com/terabyte/annaflix/homepage.java
+++ app/src/main/java/com/terabyte/annaflix/homepage.java
@@ -1,18 +1,50 @@
 package com.terabyte.annaflix;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.widget.ImageView;
 
-public class homepage extends AppCompatActivity {
+import com.gauravk.bubblenavigation.BubbleNavigationLinearView;
+import com.gauravk.bubblenavigation.listener.BubbleNavigationChangeListener;
 
+public class homepage extends AppCompatActivity {
+    BubbleNavigationLinearView bubbleNavigation;
+    Fragment selectedFragment=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
 
+        bubbleNavigation=findViewById(R.id.bottom_navigation_view_linear);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+        bubbleNavigation.setNavigationChangeListener(new BubbleNavigationChangeListener() {
+            @Override
+            public void onNavigationChanged(View view, int position) {
+                switch (position)
+                {
+                    case 0:
+                        selectedFragment=new HomeFragment();
+                        break;
+                    case 1:
+                        selectedFragment=new SearchFragment();
+                        break;
+                    case 2:
+                        selectedFragment=new NotificationsFragment();
+                        break;
+                    case 3:
+                        selectedFragment=new DownloadsFragment();
+                        break;
+                    case 4:
+                        selectedFragment=new NotificationsFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+            }
+        });
     }
 }
