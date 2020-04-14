14699c0d211109051d66d31639f0c3065cecaa1e
utsavmajhi <utsavmajhi@gmail.com>
2020-04-15 02:26:07 +0530

    added imageslider for promotions on home fragment

--- app/src/main/java/com/terabyte/annaflix/HomeFragment.java
+++ app/src/main/java/com/terabyte/annaflix/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.terabyte.annaflix;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,11 +10,17 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.smarteist.autoimageslider.DefaultSliderView;
+import com.smarteist.autoimageslider.IndicatorAnimations;
+import com.smarteist.autoimageslider.SliderLayout;
+import com.smarteist.autoimageslider.SliderView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +33,7 @@ public class HomeFragment extends Fragment {
     private HmRecrecylerAdapter mrecoanimeadpater;
     private ArrayList<recommanimitemModel> mallanimelist;
     RecyclerView hmRECOrecyclerView;
+    SliderLayout sliderLayout;
     private FirebaseFirestore db;
     public HomeFragment() {
         // Required empty public constructor
@@ -43,11 +51,59 @@ public class HomeFragment extends Fragment {
 
         LinearLayoutManager horizontalLayoutManager  = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
         hmRECOrecyclerView.setLayoutManager(horizontalLayoutManager);
+        sliderLayout = view.findViewById(R.id.imageSlider);
+        sliderLayout.setIndicatorAnimation(IndicatorAnimations.FILL); //set indicator animation by using SliderLayout.Animations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+        sliderLayout.setScrollTimeInSec(3); //set scroll delay in seconds :
+
+        setSliderViews();
 
         //set Recommended animes
         recommendedanimes();
 
         return view;
+    }
+
+    private void setSliderViews() {
+
+        for (int i = 0; i <= 3; i++) {
+
+            DefaultSliderView sliderView = new DefaultSliderView(getContext());
+
+            switch (i) {
+                case 0:
+
+                    sliderView.setImageUrl("https://firebasestorage.googleapis.com/v0/b/animedatabase-b85a7.appspot.com/o/fate.jpg?alt=media&token=f60756bb-8029-4766-bffa-10c465ccb1df");
+                    break;
+
+                case 1:
+                    sliderView.setImageUrl("https://firebasestorage.googleapis.com/v0/b/animedatabase-b85a7.appspot.com/o/charlotte.jpg?alt=media&token=fa991701-c429-436a-9f21-7c269955d3c6");
+                    break;
+                case 2:
+                    sliderView.setImageUrl("https://firebasestorage.googleapis.com/v0/b/animedatabase-b85a7.appspot.com/o/onepunch.jpg?alt=media&token=633a4807-5dec-4849-8dfe-b3cd71cd8b2f");
+                    break;
+                case 3:
+                    sliderView.setImageUrl("https://firebasestorage.googleapis.com/v0/b/animedatabase-b85a7.appspot.com/o/kimi.jpg?alt=media&token=68a01453-4381-4975-9adf-71fbc70b6915");
+                    break;
+            }
+
+            sliderView.setImageScaleType(ImageView.ScaleType.CENTER_CROP);
+            if(i==0)
+            {
+                sliderView.setDescription("Coming Soon");
+                sliderView.setDescriptionTextSize(19);
+            }
+
+            final int finalI = i;
+            sliderView.setOnSliderClickListener(new SliderView.OnSliderClickListener() {
+                @Override
+                public void onSliderClick(SliderView sliderView) {
+                    //Toast.makeText(getContext(), "This is slider " + (finalI + 1), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            //at last add this view in your layout :
+            sliderLayout.addSliderView(sliderView);
+        }
     }
 
 
@@ -77,4 +133,6 @@ public class HomeFragment extends Fragment {
         });
 
     }
+
+
 }
--- app/src/main/java/com/terabyte/annaflix/exoplayer.java
+++ app/src/main/java/com/terabyte/annaflix/exoplayer.java
@@ -16,6 +16,7 @@ import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.BandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
@@ -46,11 +47,54 @@ public class exoplayer extends AppCompatActivity {
             MediaSource mediaSource=new ExtractorMediaSource(videouri,dataSourceFactory,extractorsFactory,null,null);
             exoPlayerView.setPlayer(exoPlayer);
             exoPlayer.prepare(mediaSource);
+            exoPlayerView.setResizeMode(AspectRatioFrameLayout.RESIZE_MODE_FILL);
             exoPlayer.setPlayWhenReady(true);
 
         }catch (Exception e)
         {
             Log.e("videoplay","exoplayer error"+e.toString());
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        super.onDestroy();
+        releaseExoPlayer(exoPlayer);
+    }
+    public static void releaseExoPlayer(SimpleExoPlayer exoPlayer) {
+
+        if (exoPlayer != null) {
+            exoPlayer.release();
+
+        }
+
+    }
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+        startPlayer(exoPlayer);
+    }
+    public static void startPlayer(SimpleExoPlayer exoPlayer) {
+
+        if (exoPlayer != null) {
+            exoPlayer.setPlayWhenReady(true);
+
+        }
+    }
+    @Override
+    protected void onStop() {
+
+        super.onStop();
+        pausePlayer(exoPlayer);
+
+    }
+    public static void pausePlayer(SimpleExoPlayer exoPlayer) {
+
+        if (exoPlayer != null) {
+            exoPlayer.setPlayWhenReady(false);
+
         }
     }
 }
