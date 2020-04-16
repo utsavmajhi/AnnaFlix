71b8b441ed570d6148e5bc7374bb423baefa6e4e
utsavmajhi <utsavmajhi@gmail.com>
2020-04-17 00:28:46 +0530

    trial for download manager2

--- app/src/main/java/com/terabyte/annaflix/DownloadsFragment.java
+++ app/src/main/java/com/terabyte/annaflix/DownloadsFragment.java
@@ -2,64 +2,175 @@ package com.terabyte.annaflix;
 
 import android.app.Activity;
 import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.tonyodev.fetch2.Fetch;
-import com.tonyodev.fetch2.FetchConfiguration;
-import com.tonyodev.fetch2.NetworkType;
-import com.tonyodev.fetch2.Priority;
-import com.tonyodev.fetch2.Request;
+import com.download.library.DownloadImpl;
+import com.download.library.DownloadListener;
+import com.download.library.DownloadingListener;
+import com.download.library.Extra;
+import com.downloader.Error;
+import com.downloader.OnCancelListener;
+import com.downloader.OnDownloadListener;
+import com.downloader.OnPauseListener;
+import com.downloader.OnProgressListener;
+import com.downloader.OnStartOrResumeListener;
+import com.downloader.PRDownloader;
+import com.downloader.Progress;
+import com.huxq17.download.Pump;
+import com.huxq17.download.core.DownloadInfo;
+
+
+import java.io.File;
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class DownloadsFragment extends Fragment {
-    private Context mContext;
-    private Fetch fetch;
-
+public class DownloadsFragment extends Fragment implements DownloadListener {
+    Button b1;
     public DownloadsFragment() {
         // Required empty public constructor
-    }
-    @Override
-    public void onAttach(final Activity activity) {
-        super.onAttach(activity);
-        mContext = activity;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
        View view=inflater.inflate(R.layout.fragment_downloads, container, false);
-        FetchConfiguration fetchConfiguration = new FetchConfiguration.Builder(mContext)
-                .setDownloadConcurrentLimit(3)
-                .build();
+       b1=view.findViewById(R.id.l);
+       PRDownloader.initialize(getContext());
+       TextView t1=view.findViewById(R.id.d1txt);
+       b1.setOnClickListener(new View.OnClickListener() {
 
-        fetch = Fetch.Impl.getInstance(fetchConfiguration);
+           @Override
+           public void onClick(View view) {
 
-        String url = "https://storage.kanzaki.ru/ANIME___/Accel_World/%28Hi10%29_Accel_World_-_22_%28720p%29_%28UTW%29.mkv";
-        String file = "/downloads/test1.mkv";
+               /*
+               //Pump Downloader (Issue:Live data refreshing not showing)
+               Pump.newRequest("https://storage.kanzaki.ru/ANIME___/AKB0048/%28Hi10%29_AKB0048_-_17_%28720p%29_%28EveTaku%29.mkv")
+                       //Set id,optionally
+                       //Set DownloadTaskExecutor,optionally
+                       //Set listener,optionally
+                       .listener(new DownloadListener() {
+                           @Override
+                           public void onSuccess() {
+                               Toast.makeText(getContext(), "Successfully completed", Toast.LENGTH_SHORT).show();
+                           }
 
-        final Request request = new Request(url, file);
-        request.setPriority(Priority.HIGH);
-        request.setNetworkType(NetworkType.ALL);
-        request.addHeader("clientKey", "SD78DF93_3947&MVNGHE1WONG");
+                           @Override
+                           public void onFailed() {
+                               Toast.makeText(getContext(), "Failed", Toast.LENGTH_SHORT).show();
+                           }
 
-        fetch.enqueue(request, updatedRequest -> {
-            //Request was successfully enqueued for download.
-        }, error -> {
-            //An error occurred enqueuing the request.
-        });
+                           @Override
+                           public void onProgress(int progress) {
+                               DownloadInfo downloadInfo = getDownloadInfo();
+                               //Get download file
+                               File downloadFile = new File(downloadInfo.getFilePath());
+                               //Get download url
+                               String url = downloadInfo.getUrl();
+                               //Get download speed
+                               String speed = downloadInfo.getSpeed();
+                              // t1.setText(speed);
+                           }
+                       })
+                       .submit();
+
+               */
+               /*
+               String dirPath = getContext().getFilesDir().getAbsolutePath()+File.separator+"downloads";
+               String url="https://storage.kanzaki.ru/ANIME___/AKB0048/%28Hi10%29_AKB0048_-_17_%28720p%29_%28EveTaku%29.mkv";
+               int downloadId = PRDownloader.download(url,dirPath, "Test2.mkv")
+                       .build()
+                       .setOnStartOrResumeListener(new OnStartOrResumeListener() {
+                           @Override
+                           public void onStartOrResume() {
+
+                           }
+                       })
+                       .setOnPauseListener(new OnPauseListener() {
+                           @Override
+                           public void onPause() {
+
+                           }
+                       })
+                       .setOnCancelListener(new OnCancelListener() {
+                           @Override
+                           public void onCancel() {
+
+                           }
+                       })
+                       .setOnProgressListener(new OnProgressListener() {
+                           @Override
+                           public void onProgress(Progress progress) {
+                               String b=progress.toString();
+                               System.out.println(b);
+                           }
+                       })
+                       .start(new OnDownloadListener() {
+                           @Override
+                           public void onDownloadComplete() {
+                               System.out.println("Completed Successfully");
+                               Toast.makeText(getContext(), "Successfully downloaded", Toast.LENGTH_LONG).show();
+                           }
+
+                           @Override
+                           public void onError(Error error) {
+                               System.out.println(error);
+
+                           }
+                       });
+               */
+               DownloadImpl.getInstance()
+                       .with(getContext())
+                       .url("https://storage.kanzaki.ru/ANIME___/AKB0048/%28Hi10%29_AKB0048_-_17_%28720p%29_%28EveTaku%29.mkv")
+                       .setForceDownload(true)
+                       .enqueue(new DownloadingListener() {
+                           @Override
+                           public void onProgress(String url, long downloaded, long length, long usedTime) {
+
+                               System.out.println("Downloaded="+downloaded+"usedTime="+usedTime);
+
+                           }
+                       });
 
 
+           }
+
+       });
 
        return view;
+    }
+
+
+    private void Prdown() {
+
+    }
+
+
+    @Override
+    public void onStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength, Extra extra) {
+
+    }
+
+    @Override
+    public boolean onResult(Throwable throwable, Uri path, String url, Extra extra) {
+        return false;
     }
 }
--- app/src/main/java/com/terabyte/annaflix/homepage.java
+++ app/src/main/java/com/terabyte/annaflix/homepage.java
@@ -18,41 +18,14 @@ import android.widget.Toast;
 
 import com.gauravk.bubblenavigation.BubbleNavigationLinearView;
 import com.gauravk.bubblenavigation.listener.BubbleNavigationChangeListener;
-import com.tonyodev.fetch2.Download;
-import com.tonyodev.fetch2.Error;
-import com.tonyodev.fetch2.Fetch;
-import com.tonyodev.fetch2.FetchConfiguration;
-import com.tonyodev.fetch2.FetchListener;
-import com.tonyodev.fetch2.NetworkType;
-import com.tonyodev.fetch2.Priority;
-import com.tonyodev.fetch2.Request;
-import com.tonyodev.fetch2core.DownloadBlock;
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.io.File;
-import java.util.List;
-
-public class homepage extends AppCompatActivity implements FetchListener {
+public class homepage extends AppCompatActivity  {
     BubbleNavigationLinearView bubbleNavigation;
     Fragment selectedFragment=null;
-    private Fetch fetch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
-
-        FetchConfiguration fetchConfiguration = new FetchConfiguration.Builder(this)
-                .setDownloadConcurrentLimit(3)
-                .build();
-        Fetch fetch = Fetch.Impl.getInstance(fetchConfiguration);
-
-
-
-
-
-
         bubbleNavigation=findViewById(R.id.bottom_navigation_view_linear);
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
         bubbleNavigation.setNavigationChangeListener(new BubbleNavigationChangeListener() {
@@ -81,149 +54,4 @@ public class homepage extends AppCompatActivity implements FetchListener {
         });
     }
 
-    public void l1click(View view) {
-        Toast.makeText(this, "yes", Toast.LENGTH_SHORT).show();
-
-        String url = "https://storage.kanzaki.ru/ANIME___/Accel_World/%28Hi10%29_Accel_World_-_22_%28720p%29_%28UTW%29.mkv";
-
-        File folder=new File(Environment.getExternalStorageDirectory()+"/ANIMES");
-        if(!(folder.exists()))
-        {
-            folder.mkdir();
-        }
-        File folderanime=new File(folder,"My animes");
-        File childFile =new File(folderanime,"anime1");
-
-
-
-
-        final Request request = new Request(url, childFile.getPath()+".mkv");
-        request.setPriority(Priority.HIGH);
-        request.setNetworkType(NetworkType.ALL);
-        request.addHeader("clientKey", "SD78DF93_3947&MVNGHE1WONG");
-
-
-        fetch.enqueue(request, updatedRequest -> {
-            Toast.makeText(this, "Call is made", Toast.LENGTH_SHORT).show();
-            //Request was successfully enqueued for download.
-        }, error -> {
-            Toast.makeText(this, "Error:", Toast.LENGTH_SHORT).show();
-            //An error occurred enqueuing the request.
-        });
-        fetch.addListener(this);
-    }
-    public void l2clicl(View view) {
-    }
-
-    @Override
-    public void onAdded(@NotNull Download download) {
-        showNotification(download.getId());
-
-    }
-
-    Notification.Builder builder;
-    NotificationManagerCompat nmc;
-    private void showNotification(int id) {
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O)
-        {
-            NotificationChannel channel=new NotificationChannel("channelid1","001", NotificationManager.IMPORTANCE_DEFAULT);
-            channel.setDescription("This is Description");
-
-            NotificationManager manager=(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-            manager.createNotificationChannel(channel);
-
-            builder=new Notification.Builder(getApplicationContext(),"channelid");
-            builder.setContentTitle("Downloading File");
-            builder.setAutoCancel(false);
-            builder.setProgress(100,0,false);
-            builder.setWhen(System.currentTimeMillis());
-            builder.setPriority(Notification.PRIORITY_DEFAULT);
-
-            nmc=NotificationManagerCompat.from(getApplicationContext());
-            nmc.notify(id,builder.build());
-
-        }
-        else
-        {
-            builder=new Notification.Builder(getApplicationContext());
-            builder.setContentTitle("Downloading File");
-            builder.setAutoCancel(false);
-            builder.setProgress(100,0,false);
-            builder.setWhen(System.currentTimeMillis());
-            builder.setPriority(Notification.PRIORITY_DEFAULT);
-
-            nmc=NotificationManagerCompat.from(getApplicationContext());
-            nmc.notify(id,builder.build());
-
-        }
-    }
-
-    @Override
-    public void onCancelled(@NotNull Download download) {
-
-    }
-
-    @Override
-    public void onCompleted(@NotNull Download download) {
-        Toast.makeText(this, "Successfully Completed", Toast.LENGTH_SHORT).show();
-        if(builder!=null)
-        {
-                builder.setContentText("Download Finished");
-                builder.setProgress(0,0,false);
-                nmc.notify(download.getId(),builder.build());
-        }
-    }
-
-    @Override
-    public void onDeleted(@NotNull Download download) {
-
-    }
-
-    @Override
-    public void onDownloadBlockUpdated(@NotNull Download download, @NotNull DownloadBlock downloadBlock, int i) {
-
-    }
-
-    @Override
-    public void onError(@NotNull Download download, @NotNull Error error, @Nullable Throwable throwable) {
-
-    }
-
-    @Override
-    public void onPaused(@NotNull Download download) {
-
-    }
-
-    @Override
-    public void onProgress(@NotNull Download download, long l, long l1) {
-
-        int progress=download.getProgress();
-            builder.setProgress(100,progress,false);
-            nmc.notify(download.getId(),builder.build());
-    }
-
-    @Override
-    public void onQueued(@NotNull Download download, boolean b) {
-
-    }
-
-    @Override
-    public void onRemoved(@NotNull Download download) {
-
-    }
-
-    @Override
-    public void onResumed(@NotNull Download download) {
-
-    }
-
-    @Override
-    public void onStarted(@NotNull Download download, @NotNull List<? extends DownloadBlock> list, int i) {
-
-    }
-
-    @Override
-    public void onWaitingNetwork(@NotNull Download download) {
-
-    }
 }
